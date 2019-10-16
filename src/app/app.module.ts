b3243470a34e9a3289e3f96da97266c6178b8471
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2019-10-16 14:56:33 -0300

    adicao componente restaurante

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,21 @@ import { RouterModule } from '@angular/router';
 import {ROUTES} from './app.routes'
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { RestaurantesComponent } from './restaurantes/restaurantes.component'
-=======
+
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
->>>>>>> ef9445c7d565b0ecfe959f95f606a9113f479187
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    RestaurantesComponent
-=======
+    RestaurantesComponent,
     HeaderComponent,
     HomeComponent,
     AboutComponent
->>>>>>> ef9445c7d565b0ecfe959f95f606a9113f479187
   ],
   imports: [
     BrowserModule,
