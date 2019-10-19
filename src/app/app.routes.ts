bb804f71437c4f7675a80b46d4194955566ba5d4
Vando Araujo <tecladista.vando@gmail.com>
2019-10-19 20:09:57 -0300

    componentes das paginas de detail restaurante

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { RestaurantesComponent } from "./restaurantes/restaurantes.component";
+import { RestauranteDetailComponent } from "app/restaurante-detail/restaurante-detail.component";
 
 export const ROUTES: Routes = [
   {path: '', component: HomeComponent},
   {path: 'restaurants', component: RestaurantesComponent},
+  {path: 'restaurantes/:id', component: RestauranteDetailComponent},
   {path: 'about', component: AboutComponent}
 ]
---
+++ src/app/restaurante-detail/restaurante-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurante-detail',
+  templateUrl: './restaurante-detail.component.html'
+})
+export class RestauranteDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
