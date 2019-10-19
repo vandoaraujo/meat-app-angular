1afad32c0db0dde5f82162625a46ca6f8946278e
Vando Araujo <tecladista.vando@gmail.com>
2019-10-19 00:24:26 -0300

    correcao integracao json server

--- src/app/restaurantes/restaurantes.service.ts
+++ src/app/restaurantes/restaurantes.service.ts
@@ -1,30 +1,19 @@
+import {Injectable} from '@angular/core'
+import {Http} from  '@angular/http'
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map'
+
 import { Restaurante } from "app/restaurantes/restaurante/restaurante.model";
 
+import {MEAT_API} from '../app.api'
+
+@Injectable()
 export class RestaurantesService {
 
-  rests: Restaurante [] = [
+  constructor(private http: Http){}
 
-    {
-      id: "bread-bakery",
-      name: "Bread & Bakery",
-      category: "Bakery",
-      deliveryEstimate: "25m",
-      rating: 4.9,
-      imagePath: "assets/img/restaurants/breadbakery.png"
-    },
-    {
-      id: "burger-house",
-      name: "Burger House",
-      category: "Hamburgers",
-      deliveryEstimate: "100m",
-      rating: 3.5,
-      imagePath: "assets/img/restaurants/burgerhouse.png"
-    }
-  ]
-  
-  constructor(){}
-
-  restaurantes(): Restaurante[] {
-    return this.rests;
+  restaurantes(): Observable<Restaurante[]> {
+    return this.http.get(`${MEAT_API}/restaurants`)
+      .map(response => response.json());
   }
 }
