dce958a38e8d09728e8ba0c15eece01112c3f946
Vando Araujo <tecladista.vando@gmail.com>
2019-10-19 15:42:00 -0300

    tratamento de erro em angular

--- src/app/restaurantes/restaurantes.service.ts
+++ src/app/restaurantes/restaurantes.service.ts
@@ -2,10 +2,12 @@ import {Injectable} from '@angular/core'
 import {Http} from  '@angular/http'
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 import { Restaurante } from "app/restaurantes/restaurante/restaurante.model";
 
 import {MEAT_API} from '../app.api'
+import {ErrorHandler} from '../app.error-handler'
 
 @Injectable()
 export class RestaurantesService {
@@ -14,6 +16,7 @@ export class RestaurantesService {
 
   restaurantes(): Observable<Restaurante[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
-      .map(response => response.json());
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   }
 }
