dd8a3ebe4db698cc317d7d1d1936b8383e759d18
Vando Araujo <tecladista.vando@gmail.com>
2019-10-26 19:56:18 -0300

    formControl Reactive Forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder} from "@angular/forms"
+
 import { RadioOption } from "../shared/radio/radio-option.model";
 import { OrderService } from "app/order/order.service";
 import { CartItem } from "app/restaurante-detail/shopping-cart/cart-item.model";
@@ -11,6 +13,8 @@ import {Router} from '@angular/router'
 })
 export class OrderComponent implements OnInit {
 
+  orderForm: FormGroup
+
   delivery: number = 8
 
   paymentOptions: RadioOption[] = [
@@ -21,9 +25,20 @@ export class OrderComponent implements OnInit {
 
   value : any
 
-  constructor(private orderService: OrderService, private router : Router ) { }
+  constructor(private orderService: OrderService,
+              private router : Router,
+              private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control(''),
+      email: this.formBuilder.control(''),
+      emailConfirmation: this.formBuilder.control(''),
+      address: this.formBuilder.control(''),
+      number: this.formBuilder.control(''),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('')
+    })
   }
 
   itemsValue(): number {
