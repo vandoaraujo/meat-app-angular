ca02ca29c7c697f20793863140bcb29c5a387575
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2019-10-29 16:24:39 -0300

    animations

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations'
 
 @Component({
   selector: 'mt-snackbar',
   templateUrl: './snackbar.component.html',
-  styleUrls: ['./snackbar.component.css']
+  styleUrls: ['./snackbar.component.css'],
+  animations: [
+    trigger('snack-visibility', [
+      state('hidden', style({
+        opacity: 0,
+        bottom: '0px'
+      })),
+      state('visible', style({
+        opacity: 1,
+        bottom: '30px'
+      })),
+      transition('hidden => visible', animate('500 ms 0s ease-in')),
+      transition('visible => hidden', animate('500 ms 0s ease-out')),
+    ])
+  ]
 })
 export class SnackbarComponent implements OnInit {
 
   message: string = "Hello There"
 
+  snackVisibility: string = 'hidden'
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleSnack(){
+    this.snackVisibility = this.snackVisibility ==='hidden' ? 'visible' : 'hidden'
   }
 
 }
