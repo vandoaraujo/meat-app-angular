138c622eee5ec489e8bd3fb4eb1b14de94d57c5a
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2019-11-08 08:40:32 -0300

    router com login

--- src/app/login/loggedIn.guard.ts
+++ src/app/login/loggedIn.guard.ts
@@ -1,4 +1,4 @@
-import { Route, CanLoad } from "@angular/router/";
+import { Route, CanLoad, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from "@angular/router/";
 import { Injectable } from "@angular/core";
 import {LoginService} from './login.service'
 
@@ -7,12 +7,20 @@ export class LoggedInGuard implements CanLoad {
 
   constructor(private loginService: LoginService){}
 
-  canLoad(route: Route): boolean {
+  checkAuthentication(path: string): boolean {
     const loggedIn = this.loginService.isLoggedIn()
     if(!loggedIn){
-      this.loginService.handleLogin(`/${route.path}`)
+      this.loginService.handleLogin(`/${path}`)
     }
     return loggedIn
+  }
+
+  canLoad(route: Route): boolean {
+    return this.checkAuthentication(route.path)
+  }
+
+  canActivate(activatedRoute: ActivatedRouteSnapshot, routerState: RouterStateSnapshot): boolean {
+    return this.checkAuthentication(activatedRoute.routeConfig.path)
   }
 
 }
