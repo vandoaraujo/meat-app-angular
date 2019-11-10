dc035ca97a5a72934cf8e479a54825932028b95e
Vando Araujo <tecladista.vando@gmail.com>
2019-11-10 00:18:48 -0300

    melhoria das rotas da app

--- src/app/header/user-detail/user-detail.component.ts
+++ src/app/header/user-detail/user-detail.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from 'app/login/login.service'
+import { User } from "app/login/user.model";
 
 @Component({
   selector: 'mt-user-detail',
-  templateUrl: './user-detail.component.html'
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+  }
+
+  user(): User {
+    return this.loginService.user
+  }
+
+  isLoggedIn() : boolean {
+    return this.loginService.isLoggedIn()
+  }
+
+  login(){
+    this.loginService.handleLogin()
+  }
+
+  logout(){
+    this.loginService.logout()
   }
 
 }
