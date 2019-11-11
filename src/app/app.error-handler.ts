6dedb124431ca5c712f52ffa4f9367c66966652d
Vando Araujo <tecladista.vando@gmail.com>
2019-11-10 22:43:12 -0300

    ngZone Angular fim curso

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,4 +1,4 @@
-import {ErrorHandler, Injectable, Injector} from '@angular/core'
+import {ErrorHandler, Injectable, Injector, NgZone} from '@angular/core'
 import {Observable} from 'rxjs/Observable'
 import {HttpErrorResponse} from '@angular/common/http'
 import 'rxjs/add/observable/throw'
@@ -8,24 +8,28 @@ import { LoginService } from "app/login/login.service";
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
 
-  constructor(private ns: NotificationService, private injector: Injector){
+  constructor(private ns: NotificationService,
+              private injector: Injector,
+              private zone: NgZone){
     super()
   }
 
   handleError(errorResponse: HttpErrorResponse | any){
     if(errorResponse instanceof HttpErrorResponse){
       const message = errorResponse.error.message
-      switch(errorResponse.status){
-        case 401:
-          this.injector.get(LoginService).handleLogin()
-          break;
-        case 403:
-          this.ns.notify( message || 'Não autorizado')
-          break;
-        case 404:
-          this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.')
-          break;
-      }
+      this.zone.run(() => {
+        switch(errorResponse.status){
+          case 401:
+            this.injector.get(LoginService).handleLogin()
+            break;
+          case 403:
+            this.ns.notify( message || 'Não autorizado')
+            break;
+          case 404:
+            this.ns.notify(message || 'Recurso não encontrado. Verifique o console para mais detalhes.')
+            break;
+        }
+      })
     }
     super.handleError(errorResponse)
   }
