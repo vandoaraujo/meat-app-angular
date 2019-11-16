35e738ee810d77daaff1693e447183af5dafe177
Vando Araujo <tecladista.vando@gmail.com>
2019-11-16 16:05:12 -0300

    heroku

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/meat-app-angular'));
 
 app.get('/*', function(req,res) {
 
 //res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/meat-app-angular/index.html'));
 
 
 });
