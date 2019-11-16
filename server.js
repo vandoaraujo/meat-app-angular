108c617e3096305a4f1dd68933ef5a2c35259aec
Vando Araujo <tecladista.vando@gmail.com>
2019-11-16 16:09:47 -0300

    heroku

--- server.js
+++ server.js
@@ -5,16 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/meat-app-angular'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('/*', function(req,res) {
 
 //res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-res.sendFile(path.join(__dirname+'/dist/meat-app-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 
 
 });
-
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
